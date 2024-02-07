ca672e787e323d5b8eea8aafee92ba60d9aa2255
AhmedAHamada <91083913+AhmedAHamada@users.noreply.github.com>
2024-02-06 19:33:15 -0600

    delete wrong files

--- Meshing_KCSHull/Stages/code/Untitled2.m
+++
@@ -1,9 +0,0 @@
-clear; clc; close all;
-% Example: Define a non-linear curve (circle)
-theta = linspace(0, 2*pi, 100);
-X_original = cos(theta);
-Y_original = sin(theta);
-
-% Rediscretize the curve with 50 points
-num_points_new = 50;
-[X_new, Y_new] = rediscretize_curve(X_original, Y_original, num_points_new);
--- Meshing_KCSHull/Stages/shaft.m
+++
@@ -1,8 +0,0 @@
-clc; clear; close all;
-load('shaft.mat')
-factor1=0.2;
-ans2=ans1;
-ans2(:,1)=factor1*ans1(:,1);
-ans2(:,2)=ans2(:,2)+abs(ans2(1,2))+abs(ans2(end,2)-ans2(1,2))/2+...
-    ans1(1,2)-abs(ans1(end,2)-ans1(1,2))/2;
-plot(ans1(:,1),ans1(:,2),'k',ans2(:,1),ans2(:,2),'b')
